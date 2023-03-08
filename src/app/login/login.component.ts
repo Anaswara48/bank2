d91052b8f115d165e0a2fa5c60ee70288dcf14e0
unknown <anaswarav71@gmail.com git config --global user.email anaswarav71@gmail.co>
2023-03-08 11:09:32 +0530

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  data = "Your Perfect Banking Partner"
+  data1 = "Enter your ac number"
+  // accno=""
+  // or
+  acno: any
+  psw: any
+
+
+  userDetails: any = {
+    1000: { usename: "anu", acno: 1000, password: "abc123", balance: 0 },
+    1001: { usename: "amal", acno: 1001, password: "abc123", balance: 0 },
+    1002: { usename: "anju", acno: 1002, password: "abc123", balance: 0 },
+    1003: { usename: "arun", acno: 1003, password: "abc123", balance: 0 },
+    1004: { usename: "arul", acno: 1004, password: "abc123", balance: 0 }
+
+  }
+  constructor(private router:Router){
+
+  }
+
+  login() {
+    var acnum = this.acno
+    var psw = this.psw
+    var userDetails = this.userDetails
+    if (acnum in userDetails) {
+      if (psw == userDetails[acnum]["password"]) {
+        alert('login success')
+        //redirecting
+       this.router.navigateByUrl("dashboard")
+
+      }
+      else {
+        alert('incorrect password')
+      }
+    }
+    else {
+      alert('incorrect acno')
+    }
+    // alert("login-worked")
+  }
+
+  // acnoChange(event:any){
+  //   this.acno=event.target.value;
+  //   // console.log(this.acno);
+
+  // }
+  // pswChange(event:any){
+  //  this.psw= event.target.value
+  //  console.log(this.psw);
+
+  // }
+
+
+
+}
+
+
+
+
+
+
+
+
